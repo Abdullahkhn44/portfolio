a0a9139063fb4c531e7212b98cfb4266b5221cdd
Muhammad Abdullah <abdul148.eagle@gmail.com>
2024-08-06 10:31:39 +0500

    added text and image

--- src/sections/home.jsx
+++ src/sections/home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from '../components/header'
-import bannerimage from '../assets/images/six.jpg'
+import bannerimage from '../assets/images/neon.jpg'
 import '../assets/styles/home.css'
-import profile from '../assets/images/profile.png'
+import profile from '../assets/images/9.png'
 
 export default function Home() {
     return (
@@ -14,7 +14,9 @@ export default function Home() {
                 <div className='main'>
 
                     <div className='mainText'>
-                        <p>hi</p>
+                        <p className='hi'>Hi,i am</p>
+                        <h1 className='name'>Abdullah Khan</h1>
+                        <h1 className='dev'>React Native</h1> <h1 className='developer'>Developer</h1>
                     </div>
                     <div className='mainProfile'>
                         <img className='profileImage' src={profile} alt='banner - image' />
